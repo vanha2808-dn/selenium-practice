ec9f3b6b322e4291751c6704ef9ed1d69b56be92
Ha Van Ho Ngan <vanha2808@gmail.com>
2026-02-09 12:18:42 +0700

    [baitap3] Search

---
+++ src/main/java/org/example/amazonSearchResultPage.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class amazonSearchResultPage {
+    static WebDriver driver = new ChromeDriver();
+    private static boolean hadNotification = false;
+    public static void main(String args[]) {
+        loadAmazonHomePage();
+        checkNotification();
+        if (hadNotification) {
+            dismissNotification();
+        }
+        searchAdidasOnHomePage();
+    }
+
+    private static void loadAmazonHomePage() {
+        driver.manage().window().maximize();
+        String amazonHomePage = "https://www.amazon.com/";
+        try {
+            driver.get(amazonHomePage);
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // dismiss notification if any
+    private static void checkNotification() {
+        try {
+            WebElement notif = driver.findElement(By.xpath("//div[@class='glow-toaster-content']"));
+            hadNotification = true;
+        } catch (Exception e) {
+            hadNotification = false;
+        }
+    }
+
+    private static void dismissNotification() {
+        try {
+            WebElement notif = driver.findElement(By.xpath("//div[@class='glow-toaster-content']"));
+            WebElement dismissButton = notif.findElement(
+                    By.xpath("//span[contains(@class,'glow-toaster-button-dismiss')]")
+            );
+            dismissButton.click();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static void searchAdidasOnHomePage () {
+        try {
+            // Find Search
+            WebElement element4 = driver.findElement(
+                    By.xpath("//input[@id='twotabsearchtextbox' and @placeholder='Search Amazon']")
+            );
+            element4.sendKeys("Adidas");
+            
+
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+
+}
