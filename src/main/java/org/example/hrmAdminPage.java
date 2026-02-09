f581a92d752e57e57b204cd70d6d8730d69363c2
Ha Van Ho Ngan <vanha2808@gmail.com>
2026-02-09 09:36:52 +0700

    [baitap1] From element 1 to element 15

--- src/main/java/org/example/hrmAdminPage.java
+++ src/main/java/org/example/hrmAdminPage.java
@@ -1,10 +1,9 @@
 package org.example;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 
@@ -37,7 +36,7 @@ public class hrmAdminPage {
 
     private static void navigationAdmin() {
         try {
-            Thread.sleep(15000);
+            Thread.sleep(10000);
             WebElement adminMenuItem = driver.findElement(By.xpath("//a[@href='/web/index.php/admin/viewAdminModule']"));
 
             adminMenuItem.click();
@@ -55,17 +54,57 @@ public class hrmAdminPage {
             WebElement element1 = driver.findElement(By.xpath("//a[@href='/web/index.php/admin/viewAdminModule']"));
             WebElement element2 = driver.findElement(By.xpath("//a[@href='/web/index.php/pim/viewPimModule']"));
             WebElement element3 = driver.findElement(By.xpath("//a[@href='/web/index.php/leave/viewLeaveModule']"));
-            // system user elements
+
+            // system user - user mane
             WebElement element4 = driver.findElement(By.xpath("//div[@class='oxd-table-filter-area']//input[@class='oxd-input oxd-input--active']"));
 
+            // user role dropdown list
+            WebElement element5 = driver.findElement(By.xpath(
+                    "//div[@class='oxd-input-group__label-wrapper' and .//label[normalize-space()='User Role']]"
+                            + "/following-sibling::div"));
+            Actions actions = new Actions(driver);
+            actions.moveToElement(element5).click().perform();
+            Thread.sleep(2000);
+            WebElement element6 = driver.findElement(By.xpath("//div[@role='option']//span[normalize-space()='Admin']"));
+            WebElement element7 = driver.findElement(By.xpath("//div[@role='option']//span[normalize-space()='ESS']"));
 
+            Thread.sleep(2000);
+            actions.moveToElement(element5).click().perform();
+
+            // employee name text box
+            WebElement element8 = driver.findElement(By.xpath("//div[@class='oxd-autocomplete-text-input oxd-autocomplete-text-input--active']//input"));
+
+            // Button reset and search
+            WebElement element9 = driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--ghost']"));
+            WebElement element10 = driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']"));
+
+            // tab User management
+            WebElement element11 = driver.findElement(
+                    By.xpath("//span[@class='oxd-topbar-body-nav-tab-item' and normalize-space()='User Management']")
+            );
+
+            // get first row in information table
+            WebElement firstUserInfoRow = driver.findElement(
+                    By.xpath("//div[@class='oxd-table-card'][1]")
+            );
+
+            // get user name and user role in first row
+
+            WebElement element12 = firstUserInfoRow.findElement(By.xpath("//div[@class='oxd-table-cell oxd-padding-cell'][2]"));
+            WebElement element13 = firstUserInfoRow.findElement(By.xpath("//div[@class='oxd-table-cell oxd-padding-cell'][3]"));
+
+            // icon delete and edit in first row
+            WebElement actionCell = firstUserInfoRow.findElement(By.xpath("//div[@class='oxd-table-cell oxd-padding-cell'][6]"));
+            WebElement element14 = actionCell.findElement(By.xpath("//button[@class='oxd-icon-button oxd-table-cell-action-space']\n" +
+                    "       [.//i[@class='oxd-icon bi-trash']]"));
+            WebElement element15 = actionCell.findElement(By.xpath("//button[@class='oxd-icon-button oxd-table-cell-action-space']\n" +
+                    "       [.//i[@class='oxd-icon bi-pencil-fill']]"));
             Thread.sleep(5000);
-
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            driver.quit();
         }
     }
 }
